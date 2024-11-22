865e73fbde4e9867270a2e9d87e0ab6239334476
Yakovleva Elizaveta <lizayakovleva2005@gmail.com>
2024-11-22 23:56:20 +0200

    dowland all files

---
+++ Form1.cs
@@ -0,0 +1,481 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace L2_1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cnt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            combobox_fill();
+
+
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            
+                try
+            {
+                cnt = new SqlConnection(DB);
+                cnt.Open();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка");
+            }
+
+
+            string req = "SELECT * FROM Fuel WHERE FuelName = N'Природній газ';";
+            SqlCommand command = new SqlCommand(req, cnt);
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Перевірка наявності першого запису
+            if (!reader.Read())
+            {
+                reader.Close();
+                req = "INSERT INTO Fuel (FuelName, FuelDescription) " +
+                      "VALUES (N'Природній газ', N'Природній газ, який використовується для роботи газових турбін на ТЕС');";
+                SqlCommand cmd = new SqlCommand(req, cnt);
+
+                // Використовуємо ExecuteNonQuery для вставки даних
+                cmd.ExecuteNonQuery();
+            }
+            else
+            {
+                reader.Close();
+            }
+
+            // Перевірка наявності другого запису
+            req = "SELECT * FROM Fuel WHERE FuelName = N'Паливо';";
+            SqlCommand command2 = new SqlCommand(req, cnt);
+            SqlDataReader reader2 = command2.ExecuteReader();
+
+            if (!reader2.Read())
+            {
+                reader2.Close();
+                req = "INSERT INTO Fuel (FuelName, FuelDescription) " +
+                      "VALUES (N'Паливо', N'Викопне паливо, яке використовується для виробництва електроенергії на ТЕС');";
+                SqlCommand cmd3 = new SqlCommand(req, cnt);
+
+                // Використовуємо ExecuteNonQuery для вставки даних
+                cmd3.ExecuteNonQuery();
+            }
+            else
+            {
+                reader2.Close();
+            }
+
+            // Закриваємо з'єднання
+            cnt.Close();
+            
+        }
+        private void combobox_fill()
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            SqlConnection cnt = null;
+            try
+            {
+                cnt = new SqlConnection(DB);
+                cnt.Open();
+
+                // SQL запит для отримання даних
+                string req = "select ID from Fuel;";
+                SqlCommand command = new SqlCommand(req, cnt);
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Чистимо елементи перед додаванням
+                comboBox1.Items.Clear();
+
+                // Заповнюємо ComboBox
+                while (reader.Read())
+                {
+                    comboBox1.Items.Add(reader["ID"].ToString());
+                }
+
+                // Закриваємо рідер і з'єднання
+                reader.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Помилка");
+            }
+            finally
+            {
+                if (cnt != null)
+                {
+                    cnt.Close();
+                }
+            }
+        }
+
+
+        private void AutoSizeColumns(DataGridView dgv)
+        {
+            for (int i = 0; i < dgv.Columns.Count; i++)
+                dgv.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+        }
+        public static DataTable GetSQLTable(string Query)
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                Conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(Query, DB);
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                return dt;
+            }
+        }
+
+        
+        public static void MODIFY(string Query)
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                Conn.Open();
+                SqlCommand Comm = new SqlCommand(Query, Conn);
+                MessageBox.Show(Comm.ExecuteNonQuery().ToString());
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            AutoSizeColumns(DGV1);
+
+            if (radioButton1.Checked)
+            {
+                if (checkBox1.Checked && checkBox2.Checked)
+                {
+                    string tb = textBox5.Text;
+                    string tb1 = comboBox1.Text;
+
+                    // Перевірка на правильність ID
+                    if (!PassFormatID(tb))
+                    {
+                        MessageBox.Show("Невірний формат ID!");
+                        return;
+                    }
+
+                    DataTable T = GetSQLTable("select * FROM Plants WHERE ID = " + tb + " AND FuelName_ID = " + tb1);
+                    DGV1.DataSource = T;
+                    AutoSizeColumns(DGV1);
+                }
+                else if (checkBox1.Checked)
+                {
+                    string tb = comboBox1.Text;
+                    if (PassFormatID(tb))
+                    {
+                        DataTable T = GetSQLTable($"SELECT * FROM Plants WHERE FuelName_ID = {tb}");
+                        DGV1.DataSource = T;
+                        AutoSizeColumns(DGV1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Невірний формат ID!");
+                    }
+                }
+                else if (checkBox2.Checked)
+                {
+                    string tb = textBox5.Text;
+                    if (PassFormatID(tb))
+                    {
+                        DataTable T = GetSQLTable($"SELECT * FROM Plants WHERE ID = {tb}");
+                        DGV1.DataSource = T;
+                        AutoSizeColumns(DGV1);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Невірний формат ID!");
+                    }
+                }
+                else
+                {
+                    DataTable T = GetSQLTable("SELECT * FROM Plants INNER JOIN Fuel ON Plants.FuelName_ID = Fuel.ID");
+                    DGV1.DataSource = T;
+                    AutoSizeColumns(DGV1);
+                }
+            }
+            else if (radioButton2.Checked)
+            {
+                string PlantName = textBox4.Text;
+                string LocationPlant = textBox3.Text;
+                string FuelName_ID = comboBox1.Text;
+                string Capacity = textBox1.Text;
+                string YearCommissioned = textBox2.Text;
+
+                // Перевірка форматів
+                if (!IsValidName(PlantName) || !IsValidName(LocationPlant))
+                {
+                    MessageBox.Show("Ім'я та місце розташування повинні містити тільки букви.", "Помилка формату");
+                    return;
+                }
+
+                if (!IsValidCapacity(Capacity))
+                {
+                    MessageBox.Show("Кількість потужності повинна бути дійсним числом.", "Помилка формату");
+                    return;
+                }
+
+                if (!IsValidYear(YearCommissioned))
+                {
+                    MessageBox.Show("Рік введення повинен бути між 1900 і " + DateTime.Now.Year + ".", "Помилка формату");
+                    return;
+                }
+
+                int affectedRows = ExecuteNonQuery($"INSERT INTO Plants (PlantName, LocationPlant, FuelName_ID, Capacity, YearCommissioned) VALUES (N'{PlantName}', N'{LocationPlant}', {FuelName_ID}, {Capacity}, {YearCommissioned})");
+                MessageBox.Show($"{affectedRows} записів успішно додано.");
+            }
+            else if (radioButton3.Checked)
+            {
+                string PlantName = textBox4.Text;
+                string LocationPlant = textBox3.Text;
+                string FuelName_ID = comboBox1.Text;
+                string Capacity = textBox1.Text;
+                string YearCommissioned = textBox2.Text;
+                string ID = textBox5.Text;
+
+                // Перевірка форматів
+                if (!IsValidName(PlantName) || !IsValidName(LocationPlant))
+                {
+                    MessageBox.Show("Ім'я та місце розташування повинні містити тільки букви.", "Помилка формату");
+                    return;
+                }
+
+                if (!IsValidCapacity(Capacity))
+                {
+                    MessageBox.Show("Кількість потужності повинна бути дійсним числом.", "Помилка формату");
+                    return;
+                }
+
+                if (!IsValidYear(YearCommissioned))
+                {
+                    MessageBox.Show("Рік введення повинен бути між 1900 і " + DateTime.Now.Year + ".", "Помилка формату");
+                    return;
+                }
+
+                if (!PassFormatID(ID))
+                {
+                    MessageBox.Show("Невірний формат ID!");
+                    return;
+                }
+
+                 int affectedRows = ExecuteNonQuery($"UPDATE Plants SET PlantName=N'{PlantName}', LocationPlant=N'{LocationPlant}', FuelName_ID='{FuelName_ID}', Capacity='{Capacity}', YearCommissioned={YearCommissioned} WHERE ID ={ID};");
+                MessageBox.Show($"{affectedRows} записів успішно оновлено.");
+            }
+            else if (radioButton4.Checked)
+            {
+                string ID = textBox5.Text;
+                if (PassFormatID(ID))
+                {
+                    //MODIFY("DELETE FROM Plants WHERE ID =" + ID + ";");
+                    int affectedRows = ExecuteNonQuery($"DELETE FROM Plants WHERE ID ={ID};");
+                    MessageBox.Show($"{affectedRows} записів успішно вилучено.");
+                }
+                else
+                {
+                    MessageBox.Show("Невірний формат ID для видалення!");
+                }
+            }
+
+        }
+        private int ExecuteNonQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants'; Integrated Security=True"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+        public bool IsValidName(string name)
+        {
+            Regex regex = new Regex("^[A-Za-zА-Яа-яЁёІіЇїЄє]{1,50}$");
+            return regex.IsMatch(name);
+        }
+
+        public bool IsValidCapacity(string capacity)
+        {
+            Regex regex = new Regex("^[0-9]+$"); // Припустимо, Capacity - це дійсне число
+            return regex.IsMatch(capacity);
+        }
+
+        public bool IsValidYear(string year)
+        {
+            if (int.TryParse(year, out int parsedYear))
+            {
+                return parsedYear >= 1900 && parsedYear <= DateTime.Now.Year;
+            }
+            return false;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DGV1.DataSource = GetSQLTable();
+            //DGV1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[4].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[5].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[6].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            //DGV1.Columns[7].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+
+            AutoSizeColumns(DGV1);
+
+
+            this.Hide();
+
+            Form2 childForm = new Form2();
+            childForm.FormClosed += (s, args) =>
+            {
+                MessageBox.Show("Updating DataGridView on Form1"); // Налагодження
+                DGV1.DataSource = GetSQLTable();
+                this.Show();
+                combobox_fill();
+            };
+            childForm.Show();
+
+        }
+        public static DataTable GetSQLTable()
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            string Query = "SELECT * FROM Plants AS P INNER JOIN Fuel AS F ON P.FuelName_ID = F.ID";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                Conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(Query, DB);
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                return dt;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            string Query = "INSERT INTO Plants (PlantName, LocationPlant, FuelName_ID, Capacity, YearCommissioned) VALUES (N'ТЕС-3', N'Одеса Україна', 2, 3000, 2020)";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                Conn.Open();
+                SqlCommand Comm = new SqlCommand(Query, Conn);
+                MessageBox.Show(Comm.ExecuteNonQuery().ToString());
+
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public bool PassFormatID(string p)
+        {
+            Regex R = new Regex("[0-9]+");
+            Match M = R.Match(p);
+            return M.Success;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+
+namespace L2_1
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Form2 is closing"); // Перевірка
+            this.Close();
+
+           
+        }
+        private void AutoSizeColumns(DataGridView dgv)
+        {
+            for (int i = 0; i < dgv.Columns.Count; i++)
+                dgv.Columns[i].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+        }
+        public static DataTable GetSQLTable(string Query)
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                DataSet ds = new DataSet();
+                DataTable dt = new DataTable();
+                Conn.Open();
+                SqlDataAdapter da = new SqlDataAdapter(Query, DB);
+                da.Fill(ds);
+                dt = ds.Tables[0];
+                return dt;
+            }
+        }
+
+
+        public static void MODIFY(string Query)
+        {
+            string DB = "Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants';Integrated Security=True";
+            using (SqlConnection Conn = new SqlConnection(DB))
+            {
+                Conn.Open();
+                SqlCommand Comm = new SqlCommand(Query, Conn);
+                MessageBox.Show(Comm.ExecuteNonQuery().ToString());
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+           
+            string ID = textBox5.Text;
+            string Name = textBox4.Text;
+            string Description = textBox3.Text;
+
+            if (radioButton8.Checked) // Отримання всіх напрямків діяльності
+            {
+                DataTable T = GetSQLTable("SELECT * FROM Fuel");
+                DGV1.DataSource = T;
+                AutoSizeColumns(DGV1);
+            }
+            else if (radioButton7.Checked) // Додавання нового напрямку діяльності
+            {
+                if (!IsValidName(Name))
+                {
+                    MessageBox.Show("Назва напрямку повинна містити лише букви та бути не більше 50 символів.", "Помилка формату");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(Description))
+                {
+                    MessageBox.Show("Опис напрямку не може бути порожнім.", "Помилка формату");
+                    return;
+                }
+                int affectedRows = ExecuteNonQuery($"INSERT INTO Fuel (FuelName, FuelDescription) VALUES (N'{Name}', N'{Description}')");
+                MessageBox.Show($"{affectedRows} напрямків діяльності успішно додано!");
+            }
+            else if (radioButton6.Checked) // Оновлення існуючого напрямку діяльності
+            {
+                if (!PassFormatID(ID))
+                {
+                    MessageBox.Show("Невірний формат ID!", "Помилка формату");
+                    return;
+                }
+
+                if (!IsValidName(Name))
+                {
+                    MessageBox.Show("Назва напрямку повинна містити лише букви та бути не більше 50 символів.", "Помилка формату");
+                    return;
+                }
+
+                if (string.IsNullOrWhiteSpace(Description))
+                {
+                    MessageBox.Show("Опис напрямку не може бути порожнім.", "Помилка формату");
+                    return;
+                }
+
+                int affectedRows = ExecuteNonQuery($"UPDATE Fuel SET FuelName=N'{Name}', FuelDescription=N'{Description}' WHERE ID={ID}");
+                MessageBox.Show($"{affectedRows} напрямків діяльності успішно оновлено!");
+            }
+            else if (radioButton5.Checked) // Видалення напрямку діяльності
+            {
+                if (!PassFormatID(ID))
+                {
+                    MessageBox.Show("Невірний формат ID!", "Помилка формату");
+                    return;
+                }
+
+                int affectedRows = ExecuteNonQuery($"DELETE FROM Fuel WHERE ID={ID}");
+                MessageBox.Show($"{affectedRows} напрямків діяльності успішно вилучено!");
+            }
+        }
+        private int ExecuteNonQuery(string query)
+        {
+            using (SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB; Initial Catalog='Power Plants'; Integrated Security=True"))
+            {
+                connection.Open();
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    return command.ExecuteNonQuery();
+                }
+            }
+        }
+        public bool IsValidName(string name)
+        {
+            Regex regex = new Regex("^[A-Za-zА-Яа-яЁёІіЇїЄє]{1,50}$");
+            return regex.IsMatch(name);
+        }
+        public bool PassFormatID(string id)
+        {
+            // Перевірка, чи ID є дійсним числом
+            Regex regex = new Regex("^[0-9]+$");
+            return regex.IsMatch(id);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
